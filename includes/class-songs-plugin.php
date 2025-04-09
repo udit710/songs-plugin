35dc712842ebe9cd5956875170d8c2dfa68bcc84
udit710 <s3933905@student.rmit.edu.au>
2025-04-09 22:07:27 +1000

    Added taxonomy for plugin

--- includes/class-songs-plugin.php
+++ includes/class-songs-plugin.php
@@ -4,14 +4,20 @@ class SongsPlugin{
     public function __construct(){
 
         // Register access limits
-        add_action('admin_init', array($this, 'assign_songs_cpt_caps'));
-
+        add_action('admin_init', array($this, 'assign_caps'));
+        
         // Register custom post type
         add_action( 'init', array($this, 'songs_custom_post_type'));
+        
+        // Register taxonomy
+        add_action( 'init', array($this, 'register_genre_taxonomy'));
+        
+        // Add default term to taxonomy
+        add_action('init', array($this, 'add_default_genre_term'));
 
     }
 
-    public function assign_songs_cpt_caps() {
+    public function assign_caps() {
         $roles = array('administrator');
     
         foreach ($roles as $role_name) {
@@ -24,6 +30,10 @@ class SongsPlugin{
                 $role->add_cap('edit_others_songss');
                 $role->add_cap('publish_songss');
                 $role->add_cap('read_private_songss');
+                $role->add_cap('manage_song_genres');
+                $role->add_cap('edit_song_genres');
+                $role->add_cap('delete_song_genres');
+                $role->add_cap('assign_song_genres');
             }
         }
     }
@@ -54,8 +64,41 @@ class SongsPlugin{
             'supports' => array('title'),
         );
 
-        register_post_type('songs',$args );
+        register_post_type('songs_cpt',$args );
 
+    }
+
+    public function register_genre_taxonomy() {
+        $args = array(
+            'labels' => array(
+                'name' => 'Genre',
+                'singular_name' => 'Genre',
+                'add_new_item' => 'Add New Genre'
+            ),
+            'public' => true,
+            'hierarchical' => true,
+            'show_ui' => true,
+            'show_in_rest' => true,
+            'rewrite' => array('slug' => 'genre'),
+            'capabilities' => array(
+                'manage_terms' => 'manage_song_genres',
+                'edit_terms' => 'edit_song_genres',
+                'delete_terms' => 'delete_song_genres',
+                'assign_terms' => 'assign_song_genres',
+            ),
+        );
+
+        register_taxonomy('song_genre', 'songs_cpt', $args);
+    }
+
+    public function add_default_genre_term() {
+
+        // Add only if genre exists and term doesnt exist
+        if (taxonomy_exists( 'song_genre' )){
+            if (!term_exists('Classical', 'song_genre')) {
+                wp_insert_term('Classical', 'song_genre');
+            }
+        }
     }
     
 }
