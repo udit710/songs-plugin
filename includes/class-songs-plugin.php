158aff828eccd89fd1d4ee4903349ea854aa2e6c
udit710 <s3933905@student.rmit.edu.au>
2025-04-10 09:54:41 +1000

    Fixed restrict to admin only

--- includes/class-songs-plugin.php
+++ includes/class-songs-plugin.php
@@ -3,9 +3,6 @@
 class SongsPlugin{
     public function __construct(){
 
-        // Register access limits
-        add_action('admin_init', array($this, 'assign_caps'));
-        
         // Register custom post type
         add_action( 'init', array($this, 'register_songs_custom_post_type'));
         
@@ -14,60 +11,78 @@ class SongsPlugin{
         
         // Add default term to taxonomy
         add_action('init', array($this, 'add_default_genre_term'));
+        
+        // Register access limits
+        add_action('admin_init', array($this, 'assign_caps'));
 
     }
 
+    /**
+     * Assign relevant capabilites
+     */
     public function assign_caps() {
-        $roles = array('administrator');
-    
-        foreach ($roles as $role_name) {
-            $role = get_role($role_name);
-            if ($role) {
-                $role->add_cap('edit_songs');
-                $role->add_cap('read_songs');
-                $role->add_cap('delete_songs');
-                $role->add_cap('edit_songss');
-                $role->add_cap('edit_others_songss');
-                $role->add_cap('publish_songss');
-                $role->add_cap('read_private_songss');
-                $role->add_cap('manage_song_genres');
-                $role->add_cap('edit_song_genres');
-                $role->add_cap('delete_song_genres');
-                $role->add_cap('assign_song_genres');
-            }
+        $role = get_role('administrator');
+        $capabilities = array(
+            'edit_song',
+            'read_song',
+            'delete_song',
+            'edit_songs',
+            'edit_others_songs',
+            'delete_songs',
+            'publish_songs',
+            'read_private_songs',
+            'delete_private_songs',
+            'delete_published_songs',
+            'delete_others_songs',
+            'edit_private_songs',
+            'edit_published_songs',
+            'manage_song_genres',
+            'edit_song_genres',
+            'delete_song_genres',
+            'assign_song_genres',
+        );
+
+        // Assign admin capabilites
+        foreach ( $capabilities as $cap ) {
+            $role->add_cap( $cap );
         }
     }
 
+    /**
+     * Register the custom post type 'Songs'
+     */
     public function register_songs_custom_post_type(){
         $args = array(
             'public' => true,
-            'has_archive' => true,
-            'supports' => array('title'),
-            'exclude_from_search' => true,
+            'has_archive' => true,            
+            'exclude_from_search' => false,
             'publicly_queryable' => false,
-            'capability_type' => 'songs',
-            'map_meta_cap' => true,
-            'capabilities' => array(
-                'edit_post' => 'edit_songs',
-                'read_post' => 'read_songs',
-                'delete_post' => 'delete_songs',
-                'edit_posts' => 'edit_songss',
-                'edit_others_posts' => 'edit_others_songss',
-                'publish_posts' => 'publish_songss',
-                'read_private_posts' => 'read_private_songss',
-            ),
+            'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'custom-fields'),
+            'show_ui' => true,
+            'capability_type' => array('song', 'songs'),
             'labels' => array(
                 'name' => 'Songs',
-                'singular_name' => 'Songs Entry'
+                'singular_name' => 'Song',
+                'add_new_item' => 'Add New Song',
+                'edit_item' => 'Edit Song',
+                'view_item' => 'View Song',
+                'view_items' => 'View Song',
+                'delete_item' => 'Delete Song',
+                'search_items' => 'Search Songs',
+                'not_found' => 'No Songs found',
+                'not_found_in_trash' => 'No Songs found in Trash',
             ),
             'menu_icon' => 'dashicons-playlist-audio',
-            'supports' => array('title'),
+            'map_meta_cap' => true,
         );
 
         register_post_type('songs_cpt',$args );
 
     }
 
+    /**
+     * Register the Taxonomy 'Genre' for the custom post type 'Songs'
+     */
     public function register_genre_taxonomy() {
         $args = array(
             'labels' => array(
@@ -86,11 +101,15 @@ class SongsPlugin{
                 'delete_terms' => 'delete_song_genres',
                 'assign_terms' => 'assign_song_genres',
             ),
+            'map_meta_cap' => true,
         );
 
         register_taxonomy('song_genre', 'songs_cpt', $args);
     }
 
+    /**
+     * Add default term to Taxonomy 'Genre'
+     */
     public function add_default_genre_term() {
 
         // Add only if genre exists and term doesnt exist
